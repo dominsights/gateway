3c56562d28a37e93b47ea0a21ca8df7442ee9797
Domício Medeiros <domicioam@gmail.com>
2020-04-12 18:03:55 +0100

    Adding project to mock external api.

---
+++ BankSimulator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace BankSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stub = FluentMockServer.Start(new FluentMockServerSettings
+            {
+                Urls = new[] { "http://+:5001" },
+                StartAdminInterface = true
+            });
+
+            stub.Given(
+                Request.Create()
+                .WithPath("/helloworld")
+                .UsingGet())
+                .RespondWith(Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "application/json")
+                .WithBodyAsJson(new { Message = "Hello world" }));
+
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadLine();
+            stub.Stop();
+        }
+    }
+}
